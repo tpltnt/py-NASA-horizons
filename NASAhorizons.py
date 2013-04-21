fcf0154a99c202d7aefeadd6cfdeba28f5a5b853
tpltnt <tpltnt.github@dropcut.net>
2013-04-21 11:19:29 +0200

    telnet session creation added

--- NASAhorizons.py
+++ NASAhorizons.py
@@ -2,6 +2,20 @@
 
 class NASAhorizons(object):
     """a python wrapper for the NASA HORIZONS data service telnet interface"""
+    import telnetlib
 
+    __telnetsession = None
     def __init__(self):
         print("foo")
+
+    def create_telnetsession(self):
+        """Creates a new telnet connection to the NASA HORIZONS data service.
+        Calling this method *will destroy* your old connection."""
+        host    = "horizons.jpl.nasa.gov"
+        port    = 6775
+        timeout = 9999
+
+        try:
+            self.__telnetsession = telnetlib.Telnet(host, port, timeout)
+        except socket.timeout:
+            raise Exception("socket timed out")
