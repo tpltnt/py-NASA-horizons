10445d24537f8da7c11f5cf11db72e38f7a9b912
tpltnt <tpltnt.github@dropcut.net>
2013-04-21 10:53:33 +0200

    first code added

---
+++ py-NASA-HORIZONS.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+class NASAhorizons(object):
+    """a python wrapper for the NASA HORIZONS data service telnet interface"""
+
+    def __init__(self):
+        print("foo")
