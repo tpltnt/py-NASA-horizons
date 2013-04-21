09fe5f4a8621a27fa07b26584bed6cbbfbd24b63
tpltnt <tpltnt.github@dropcut.net>
2013-04-21 11:45:29 +0200

    test for session creation on init added

--- tests/test_NASAhorizons.py
+++ tests/test_NASAhorizons.py
@@ -9,6 +9,12 @@ def test_init0():
     foo = NASAhorizons()
 
 
+def test_init1():
+    """test if object construction already creates a session (as expected)."""
+    foo = NASAhorizons()
+    assert foo.has_session() is True
+
+
 def test_create_telnetsession():
     """test to create a telnet session. This should not time out."""
     foo = NASAhorizons()
