8c03b51d925ae2d367bc95e5805dcb75a581bb97
tpltnt <tpltnt.github@dropcut.net>
2013-04-21 11:30:20 +0200

    logical fixed

--- NASAhorizons.py
+++ NASAhorizons.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import socket
+import telnetlib
 
 class NASAhorizons(object):
     """a python wrapper for the NASA HORIZONS data service telnet interface"""
-    import telnetlib
 
     __telnetsession = None
     def __init__(self):
@@ -11,6 +12,7 @@ class NASAhorizons(object):
     def create_telnetsession(self):
         """Creates a new telnet connection to the NASA HORIZONS data service.
         Calling this method *will destroy* your old connection."""
+        
         host    = "horizons.jpl.nasa.gov"
         port    = 6775
         timeout = 9999
@@ -18,4 +20,4 @@ class NASAhorizons(object):
         try:
             self.__telnetsession = telnetlib.Telnet(host, port, timeout)
         except socket.timeout:
-            raise Exception("socket timed out")
+            raise IOError("socket timed out")
