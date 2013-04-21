75da353f574067754067a11595f383594bb436a2
tpltnt <tpltnt.github@dropcut.net>
2013-04-21 11:29:54 +0200

    test for telnetsession creation added

--- tests/test_NASAhorizons.py
+++ tests/test_NASAhorizons.py
@@ -7,3 +7,9 @@ from NASAhorizons import NASAhorizons
 def test_init0():
     """test object construction"""
     foo = NASAhorizons()
+
+
+def test_create_telnetsession():
+    """test to create a telnet session. This should not time out."""
+    foo = NASAhorizons()
+    foo.create_telnetsession()
