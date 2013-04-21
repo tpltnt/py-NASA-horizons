3203c23e6273a099debf2b7b4ddf8d40734b1996
tpltnt <tpltnt.github@dropcut.net>
2013-04-21 10:58:59 +0200

    cleaning up names

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,9 @@
 import sys
 sys.path.append('../py-NASA-horizons')
 import pytest
-from py-NASA-horizons import NASAhorizons
+from NASAhorizons import NASAhorizons
 
 
 def test_init0():
     """test object construction"""
     foo = NASAhorizons()
-
